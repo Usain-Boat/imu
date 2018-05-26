96dbd363fc470fddcacc577e2be842823a9ca626
jamiecheng <jamiecheng97@gmail.com>
2018-05-26 09:54:43 +0200

    changed lib

--- src/main.cpp
+++ src/main.cpp
@@ -3,46 +3,6 @@
 
 #include "usain_imu.h"
 
-// refresh time. set to 500 for part 2 and 50 for part 4
-#define REFRESH_TIME_MS 50
-
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
-{
-  float roll = atan2(ay, az);
-  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
-
-  // Calculate heading
-  float heading = atan2(my, mx);
-
-  // Set declination angle on your location and fix heading
-  // You can find your declination on: http://magnetic-declination.com/
-  // (+) Positive or (-) for negative
-  // For Bytom / Poland declination angle is 4'26E (positive)
-  // Formula: (deg + (min / 60.0)) / (180 / M_PI);
-  float declinationAngle = (1.0 + (35.0 / 60.0)) / (180 / M_PI);
-  heading += declinationAngle;
-
-  // Correct for heading < 0deg and heading > 360deg
-  if (heading < 0)
-  {
-    heading += 2 * PI;
-  }
-
-  if (heading > 2 * PI)
-  {
-    heading -= 2 * PI;
-  }
-
-  // Convert everything from radians to degrees:
-  heading *= 180.0 / PI;
-  pitch *= 180.0 / PI;
-  roll *= 180.0 / PI;
-
-//    printf("pitch: %f\n", pitch);
-//    printf("roll: %f\n", roll);
-  printf("heading: %f\n", heading);
-}
-
 void isr()
 {
   printf("collision\n");
@@ -57,17 +17,11 @@ int main()
     error("error: no imu connected\n");
   }
 
-  imu.register_on_collision(callback(isr));
+//  imu.register_on_collision(callback(isr));
 
   while (true)
   {
-//        printf("running...\n");
-//        printf("pitch: %2f\n", imu.get_pitch());
-//        printf("G: %2f, %2f, %2f\r\n", imu.gx, imu.gy, imu.gz);
-//        printf("M: %2f, %2f, %2f\r\n\r\n", imu.mx, imu.my, imu.mz);
-//
-//        printAttitude(imu.ax, imu.ay, imu.az, imu.mx, imu.my, imu.mz);
-
-    wait(3);
+    printf("heading: %f\n", imu.get_compass());
+    wait(0.5);
   }
 }
--- src/usain_imu.cpp
+++ src/usain_imu.cpp
@@ -5,31 +5,16 @@
 #include <arm_math.h>
 #include "usain_imu.h"
 
+
 UsainIMU::UsainIMU() :
-    _imu(D14, D15)
+    _imu(0xD6, 0x3C)
 {
 
 }
 
 int UsainIMU::init()
 {
-  // gyro.latchInterrupt controls the latching of the
-  // gyro and accelerometer interrupts (INT1 and INT2).
-  // false = no latching
-  _imu.settings.gyro.latchInterrupt = 0;
-
-  // Set gyroscope scale to +/-245 dps:
-  _imu.settings.gyro.scale = 245;
-  // Set gyroscope (and accel) sample rate to 14.9 Hz
-  _imu.settings.gyro.sampleRate = 1;
-  // Set accelerometer scale to +/-2g
-  _imu.settings.accel.scale = 2;
-  // Set magnetometer scale to +/- 4g
-  _imu.settings.mag.scale = 4;
-  // Set magnetometer sample rate to 0.625 Hz
-  _imu.settings.mag.sampleRate = 0;
-
-  uint16_t status = _imu.begin();
+  uint16_t status = _imu.init();
 
   if (status == 0)
   {
@@ -43,20 +28,17 @@ int UsainIMU::init()
 
 float UsainIMU::get_compass()
 {
-  return 0;
+  return _imu.getHeading();
 }
 
 float UsainIMU::get_pitch()
 {
-  float pitch = atan2(-_imu.ax, sqrt(_imu.ay * _imu.ay + _imu.az * _imu.az));
-  pitch *= 180.0 / PI;
-
-  return pitch;
+  return _imu.getPitch();
 }
 
 float UsainIMU::get_roll()
 {
-  return 0;
+  return _imu.getRoll();
 }
 
 void UsainIMU::register_on_collision(const Callback<void()> &callback)
@@ -71,20 +53,12 @@ void UsainIMU::collision_thread()
 
   while (true)
   {
-    _imu.readAccel();
-    _imu.readGyro();
-    _imu.readMag();
+    _imu.update();
 
-//    printf("ax: %d   ay:%d\n", _imu.ax, _imu.ay);
-
-//    if (_imu.ax > 1000 || _imu.ax < -1000 || _imu.ay > 1000 || _imu.ay < -1000)
-//    {
-//      printf("collision!\n");
-//    }
-
-    if(abs(_imu.ax - prev_ax) > 6500 || abs(_imu.ay - prev_ay) > 6500)
+    if (abs(_imu.ax - prev_ax) > 6500 || abs(_imu.ay - prev_ay) > 6500)
     {
-      _colliion_callback.call();
+      if(_colliion_callback)
+        _colliion_callback.call();
     }
 
     prev_ax = _imu.ax;
